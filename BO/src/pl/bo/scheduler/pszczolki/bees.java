cd80a2244737472bc22421b102ed9f70e51d6346
Michal <Michal@Kus>
2013-11-11 21:02:56 +0100

    nic

---
+++ BO/src/pl/bo/scheduler/pszczolki/bees.java
@@ -0,0 +1,23 @@
+package pl.bo.scheduler.pszczolki;
+
+import java.util.List;
+
+public class bees {
+
+	List<Zadanie> _listaZadan;
+	int _worker;
+	int _scout;
+	
+	public void scouting(){
+		
+	}
+	
+	public void working(){
+		
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
